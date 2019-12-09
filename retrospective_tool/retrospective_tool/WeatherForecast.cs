b36ab8310b687f4f2d9b67180078cdb18daa1c82
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-12-09 11:32:05 +0100

    Project Setup (Template Angular + .NET core 3.0)

---
+++ retrospective_tool/retrospective_tool/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace retrospective_tool
+{
+	public class WeatherForecast
+	{
+		public DateTime Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+		public string Summary { get; set; }
+	}
+}
