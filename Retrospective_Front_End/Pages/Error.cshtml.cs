59c1b6ac0fdff0fcd26ae0bae0674f7b5dbde8b5
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-12-09 18:26:24 +0100

    Split front and backend

--- Retrospective_Front_End/Pages/Error.cshtml.cs
+++ Retrospective_Front_End/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace retrospective_tool.Pages
+namespace Retrospective_Front_End.Pages
 {
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	public class ErrorModel : PageModel
--- Retrospective_Front_End/Program.cs
+++ Retrospective_Front_End/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace retrospective_tool
+namespace Retrospective_Front_End
 {
 	public class Program
 	{
