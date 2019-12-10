2947bc6fd0b0d5a3a327e6a525ace63dace4a65f
robin <playwarrior2@gmail.com>
2019-12-10 12:24:48 +0100

    Creation of EntityFramework

---
+++ RetroSpective.EFSQLRetroSpectiveDbImpl/RetroSpectiveDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetroSpective.Core.Models;
+using System;
+
+namespace RetroSpective.EFSQLRetroSpectiveDbImpl {
+    public class RetroSpectiveDbContext : DbContext {
+
+        public RetroSpectiveDbContext(DbContextOptions<RetroSpectiveDbContext> options) : base(options) { }
+
+        public DbSet<Participant> Participants { get; set; }
+
+        public DbSet<Facilitator> Facilitators { get; set; }
+
+        public DbSet<Core.Models.RetroSpective> RetroSpectives { get; set; }
+
+        public DbSet<RetroColumn> RetroColumns { get; set; }
+
+        public DbSet<RetroCard> RetroCards { get; set; }
+    }
+}
