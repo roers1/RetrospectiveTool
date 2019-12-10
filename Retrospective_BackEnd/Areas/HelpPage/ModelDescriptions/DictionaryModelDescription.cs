1d0a74a612c92d6f57785f378bd04f12c322e73f
Dilivio Chin-A-Teh <drf.chin-a-teh@student.avans.nl>
2019-12-10 12:40:00 +0100

    Test endpoint works with fake repo.

--- Retrospective_BackEnd/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Retrospective_BackEnd.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Retrospective_BackEnd/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Retrospective_BackEnd.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
