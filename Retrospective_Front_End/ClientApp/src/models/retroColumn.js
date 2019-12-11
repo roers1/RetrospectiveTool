062659339dc2580e5a941aa152c0aabf758eca05
Brion <khobryan2000@gmail.com>
2019-12-11 14:08:24 +0100

    Added id to retroColumn

---
+++ Retrospective_Front_End/ClientApp/src/models/retroColumn.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var RetroColumn = /** @class */ (function () {
+    function RetroColumn() {
+    }
+    return RetroColumn;
+}());
+exports.RetroColumn = RetroColumn;
+//# sourceMappingURL=retroColumn.js.map
