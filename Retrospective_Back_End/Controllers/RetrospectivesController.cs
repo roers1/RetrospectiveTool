e4e882c7e58ff74d737d7b59ebf4a38ae74c806c
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-12-12 16:06:54 +0100

    Refactoring and deleting of namespaces

--- Retrospective_Back_End/Controllers/RetrospectivesController.cs
+++ Retrospective_Back_End/Controllers/RetrospectivesController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Retrospective_Core.Models;
 using Retrospective_Core.Services;
-using Retrospective_EFSQLRetrospectiveDbImpl;
+using Retrospective_Core.Models;
 
 namespace Retrospective_Back_End.Controllers
 {
