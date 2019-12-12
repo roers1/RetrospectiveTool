163b8b60c36468ceecea4e17b4e15c03a800831c
Ruben van Oers <r.vanoers1@student.avans.nl>
2019-12-12 09:28:05 +0100

    Improved constraints

---
+++ RetroSpective.EFSQLRetroSpectiveDbImpl/Migrations/20191212082642_added cascade constraint.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retrospective_EFSQLRetroSpectiveDbImpl.Migrations
+{
+    public partial class addedcascadeconstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
