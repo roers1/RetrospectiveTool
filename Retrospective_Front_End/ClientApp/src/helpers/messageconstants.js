c502ba203b0eb14451458794827945e4aada17cd
Brion <khobryan2000@gmail.com>
2019-12-16 13:35:14 +0100

    Added constants for alert and general messages.

---
+++ Retrospective_Front_End/ClientApp/src/helpers/messageconstants.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RETROBOARD_CLEAN_ACTION_CONFIRM = 'Weet je zeker dat je jouw Retro Board wilt opschonen? (Kan niet ongedaan gemaakt worden!)';
+exports.RETROBOARD_CLEAN_ERROR_ALREADY = 'Jouw Retro Board is al leeg!';
+//# sourceMappingURL=messageconstants.js.map
---
+++ Retrospective_Front_End/ClientApp/src/helpers/messageconstants.ts
@@ -0,0 +1,2 @@
+export const RETROBOARD_CLEAN_ACTION_CONFIRM = 'Weet je zeker dat je jouw Retro Board wilt opschonen? (Kan niet ongedaan gemaakt worden!)';
+export const RETROBOARD_CLEAN_ERROR_ALREADY = 'Jouw Retro Board is al leeg!';
