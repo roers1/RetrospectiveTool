b50390a056aabf7cade2f09f69a19c0ebf29f23c
Brion <khobryan2000@gmail.com>
2019-12-17 14:16:52 +0100

    Added updateCard func

--- Retrospective_Core/Services/IRetroRespectiveRepository.cs
+++ Retrospective_Core/Services/IRetroRespectiveRepository.cs
@@ -2,17 +2,20 @@
 using System.Linq;
 using Retrospective_Core.Models;
 
-namespace Retrospective_Core.Services {
-    public interface IRetroRespectiveRepository {
+namespace Retrospective_Core.Services
+{
+    public interface IRetroRespectiveRepository
+    {
 
-	    IQueryable<Retrospective> Retrospectives { get; }
-	    IQueryable<RetroColumn> RetroColumns { get; }
-	    IQueryable<RetroCard> RetroCards { get; }
-	    void SaveRetroCard(RetroCard retroCard);
-		void RemoveRetroCard(RetroCard retroCard);
-	    void SaveRetroColumn(RetroColumn retroColumn);
-	    void RemoveRetroColumn(RetroColumn retroColumn);
-	    void SaveRetrospective(Retrospective retrospective);
-	    void RemoveRetrospective(Retrospective retrospective);
+        IQueryable<Retrospective> Retrospectives { get; }
+        IQueryable<RetroColumn> RetroColumns { get; }
+        IQueryable<RetroCard> RetroCards { get; }
+        void SaveRetroCard(RetroCard retroCard);
+        RetroCard UpdateRetroCard(RetroCard retroCard);
+        void RemoveRetroCard(RetroCard retroCard);
+        void SaveRetroColumn(RetroColumn retroColumn);
+        void RemoveRetroColumn(RetroColumn retroColumn);
+        void SaveRetrospective(Retrospective retrospective);
+        void RemoveRetrospective(Retrospective retrospective);
     }
 }
