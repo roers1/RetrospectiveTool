8a0c8552653e6554024ef0fd8a55898064eb07ce
Dilie <dilie_90@hotmail.com>
2019-12-17 14:05:47 +0100

    Refactored the Retrospective test.

--- Retrospective_Back_End_Test/TestRetrospectiveController.cs
+++ Retrospective_Back_End_Test/TestRetrospectiveController.cs
@@ -16,32 +16,31 @@ namespace Retrospective_Back_End_Test
 {
     public class TestRetrospectiveController
     {
-        [Fact]
-        public async void getAllRetrospectives()
+        Mock<IRetroRespectiveRepository> mockRetrospectiveRepo;
+        IList<Retrospective> retrospectives;
+        public TestRetrospectiveController()
         {
-            //Arrange
-            String testTitle = "Board 1";
-            Mock<IRetroRespectiveRepository> mockRetrospectiveRepo = new Mock<IRetroRespectiveRepository>();
-            var retrospectives = new List<Retrospective>()
+           this.mockRetrospectiveRepo = new Mock<IRetroRespectiveRepository>();
+            this.retrospectives = new List<Retrospective>()
             {
-                new Retrospective { 
-                    Title = testTitle,
+                new Retrospective {
+                    Title = "Board 1",
                     Description = "Dit is board 1",
                     CreatedDate = DateTime.Now,
                     RetroColumns = new List<RetroColumn>()
                     {
                         new RetroColumn
-                        { 
+                        {
                             Title = "Kolom 1",
                             RetroCards = new List<RetroCard>()
                             {
-                                new RetroCard 
+                                new RetroCard
                                 {
                                     Content = "Dit is kaart 1",
                                     Position = 1
                                 }
                             }
-                        } 
+                        }
                     }
 
                 },
@@ -51,6 +50,13 @@ namespace Retrospective_Back_End_Test
                     Description = "Dit is board 2"
                 }
             };
+        }
+
+        [Fact]
+        public async void getAllRetrospectives()
+        {
+            //Arrange
+            String testTitle = "Board 1";
             
             mockRetrospectiveRepo.Setup(m => m.getAll()).Returns(retrospectives.AsQueryable());
             var controller = new RetrospectivesController(mockRetrospectiveRepo.Object);
@@ -58,6 +64,7 @@ namespace Retrospective_Back_End_Test
             //Act
            var result = await controller.GetRetrospectives();
 
+            //Assert
            Console.WriteLine(result);
            var test = result.Value.FirstOrDefault().Title;
            Assert.True(test.Equals("Board 1"));
