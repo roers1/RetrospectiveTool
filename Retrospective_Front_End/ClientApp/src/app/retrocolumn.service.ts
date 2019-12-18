9675e2b2442856c724046cfa9ba10a15c71be061
Brion <khobryan2000@gmail.com>
2019-12-18 10:48:00 +0100

    Added updateColumn to retrocolumn service

--- Retrospective_Front_End/ClientApp/src/app/retrocolumn.service.ts
+++ Retrospective_Front_End/ClientApp/src/app/retrocolumn.service.ts
@@ -1,10 +1,10 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {BASE_URL} from '../helpers/urlconstants';
-import {MessageService} from './message.service';
-import {RetroColumn} from '../models/retroColumn';
-import {RetrospectiveService} from './retrospective.service';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { BASE_URL } from '../helpers/urlconstants';
+import { MessageService } from './message.service';
+import { RetroColumn } from '../models/retroColumn';
+import { RetrospectiveService } from './retrospective.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ import {RetrospectiveService} from './retrospective.service';
 export class RetrocolumnService {
   private readonly baseUrlRetrospective: string = BASE_URL + 'retrospectives/';
 
-  private readonly baseUrlRetroColumn = BASE_URL + 'retrocolumns';
+  private readonly baseUrlRetroColumn = BASE_URL + 'retrocolumns/';
 
   private httpOptions = {
     headers: new HttpHeaders({
@@ -40,9 +40,17 @@ export class RetrocolumnService {
     }
   }
 
+  updateColumn(retroColumnId: number, title: string) {
+    if (this.retrospectiveService.getCurrentRetrospective()) {
+      return this.http.put<RetroColumn>(this.baseUrlRetroColumn + retroColumnId, {
+        title: title
+      }, this.httpOptions);
+    }
+  }
+
   removeColumn(columnId): Observable<RetroColumn> {
     if (this.retrospectiveService.getCurrentRetrospective()) {
-      return this.http.delete<RetroColumn>(this.baseUrlRetroColumn + '/' + columnId, this.httpOptions);
+      return this.http.delete<RetroColumn>(this.baseUrlRetroColumn + columnId, this.httpOptions);
     }
   }
 }
